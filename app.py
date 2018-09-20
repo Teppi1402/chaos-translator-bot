1fd44f24c7f2c6926a8e84b069c5ad2e071fe726
Teppi1402 <43402522+Teppi1402@users.noreply.github.com>
2018-09-20 23:45:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,10 +89,10 @@ def handle_message(event):
                 contents=[TextComponent(text=translated, weight='bold')]
             ),
     )
-    flexmsg = FlexSendMessage(alt_text="", contents=bubble)
+    
     line_bot_api.reply_message(
             event.reply_token,
-            flexmsg)
+            FlexSendMessage(alt_text="", contents=bubble))
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
