7457e1892284234c9e0bc91d52ba9bc523711b46
Teppi1402 <43402522+Teppi1402@users.noreply.github.com>
2018-09-21 09:52:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    LineBotApiError, InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,    
@@ -43,12 +43,7 @@ def callback():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)    
-    except LineBotApiError as e:
-        print("Got exception from LINE Messaging API: %s\n" % e.message)
-        for m in e.error.details:
-            print("  %s: %s" % (m.property, m.message))
-        print("\n")
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
