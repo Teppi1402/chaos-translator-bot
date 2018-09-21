b3902bc07672e70493a9f52ec2d8a5efe52470b6
Teppi1402 <43402522+Teppi1402@users.noreply.github.com>
2018-09-21 19:48:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ translator = Translator()
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+line_bot_api = LineBotApi('xV4mgKpwcK4p1fOCAdOH2IXFmyRgClO+oaG7+xtWsd8x9ZrVCWifmwOYtm+k6s1JFwn3+5IEvxgEIQX3SrB462J9/FrwEXO1vllaiL5jbce+6Ce2WEJDwhY8vWwr46wgs0CADAq/RLxrDRtDjlU9jQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('e4881dd59268051feae22f38584cded1')
 
 @app.route('/')
 def homepage():
