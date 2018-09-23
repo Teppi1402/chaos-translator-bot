90859be83e8117f3e33e651fc7b5520e7cc7a0b0
Teppi1402 <43402522+Teppi1402@users.noreply.github.com>
2018-09-23 20:38:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def translate_text(text):
     trans_text = ""
     if lang == "en":
         tb.correct()
-        trans_text = str(translate(to='vi'))
+        trans_text = str(tb.translate(to='vi'))
     else:
-        trans_text = str(translate(to='en'))    
+        trans_text = str(tb.translate(to='en'))    
     return trans_text
 
 @handler.add(MessageEvent, message=TextMessage)
